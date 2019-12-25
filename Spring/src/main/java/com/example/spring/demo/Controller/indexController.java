8a2f2097f8827b4333440bdda0c2b9d73f22f5d7
1594865160 <1594865160@qq.com>
2019-12-25 21:07:30 +0800

    add index and bootstrap

--- Spring/src/main/java/com/example/spring/demo/Controller/indexController.java
+++ Spring/src/main/java/com/example/spring/demo/Controller/indexController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
-public class HelloController {
-    @GetMapping("/Hello")
-    public String Hello(@RequestParam(name="name") String name, Model model) {
-        model.addAttribute("name",name);
-        return "Hello";
+public class indexController {
+    @GetMapping("/")
+    public String index(){
+
+        return "index";
     }
 }
