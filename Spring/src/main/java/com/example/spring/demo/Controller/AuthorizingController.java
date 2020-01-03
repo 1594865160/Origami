18bd4985569ed3d947afd4047d6f80fc24b5abe6
1594865160 <1594865160@qq.com>
2020-01-03 13:53:35 +0800

    2020.1.3Cookie and Session

--- Spring/src/main/java/com/example/spring/demo/Controller/AuthorizingController.java
+++ Spring/src/main/java/com/example/spring/demo/Controller/AuthorizingController.java
@@ -1,13 +1,21 @@
 package com.example.spring.demo.Controller;
 
+import com.example.spring.demo.Mapper.UserMapper;
 import com.example.spring.demo.Provider.GithubProvider;
 import com.example.spring.demo.dto.AccessTokenDTO;
 import com.example.spring.demo.dto.GithubUser;
+import com.example.spring.demo.model.User;
+import org.omg.PortableInterceptor.USER_EXCEPTION;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.UUID;
 
 @Controller
 public class AuthorizingController {
@@ -20,9 +28,15 @@ public class AuthorizingController {
     private String clientSecret;
     @Value("${github.Redirect.uri}")
     private String Redirectsuri;
+    @Autowired
+    private UserMapper userMapper;
+
     @GetMapping("/callback")
+
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request,
+                           HttpServletResponse response) {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setCode(code);
         accessTokenDTO.getRedirect_uri(Redirectsuri);
@@ -30,9 +44,24 @@ public class AuthorizingController {
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getName());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            String token = UUID.randomUUID().toString();
+            user.setToken(token);
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            response.addCookie(new Cookie("token",token));
+//            request.getSession().setAttribute("user", githubUser);
+            return "redirect:/";
+            //登陆成功
+        }else {
+            //登陆失败
+            return "redirect:/";
+        }
     }
 
 }
